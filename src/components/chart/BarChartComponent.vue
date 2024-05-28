14e902c06bcf7b3ab448195cd4b98b14f0e23d1b
Alvin <iglu123@naver.com>
2024-05-29 01:30:27 +0900

    Add vue-chartjs plugin for chart components
    This commit includes the following changes:
    - Added BarChartComponent.vue to display bar charts
    - Added ChartView.vue for chart visualization
    - Implemented basic chart display functionality in ChartView.vue

---
+++ src/components/chart/BarChartComponent.vue
@@ -0,0 +1,56 @@
+<template>
+  
+  <Bar :data="data" :options="options" />
+</template>
+
+<script>
+import { Bar } from 'vue-chartjs';
+
+
+export default {
+  name: 'BarChart',
+  data() {
+    return {
+      data: {
+        labels: [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ],
+  datasets: [
+    {
+      label: 'Data One',
+      backgroundColor: '#f87979',
+      data: [40, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11]
+    }
+  ]
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false
+      }
+    };
+  },
+  components: {
+    Bar
+  },
+  mounted() {
+    // this.updateChartData();
+    // setInterval(this.updateChartData, 3000);
+  },
+  methods: {
+    // updateChartData() {
+    //   this.data = chartConfig.randomData();
+    // }
+  }
+};
+</script>
---
+++ src/views/ChartView.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <BarChart></BarChart>
+    </div>
+</template>
+
+<script>
+import BarChart from '@/components/chart/BarChartComponent.vue'
+    export default {
+        components: {
+            BarChart,
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
